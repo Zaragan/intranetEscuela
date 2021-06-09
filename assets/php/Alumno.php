216ebc4880562726874869f6df9e5c6a873da088
Erik <erikpvc@gmail.com>
2021-06-09 05:16:40 +0200

    Ya lo complicaremos más adelante

--- assets/php/Alumno.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Alumno  {
-    static private $nombre, $apellido, $dni, $correo, $nacido, $curso;
-    static private $notas = array('matematicas' => 0, 'ingles' => 0, 'lenguaje' => 0, 'sociales' => 0, 'ciencias' => 0);
-    
-}
-$notas['matematicas'] = 1;
-?>
