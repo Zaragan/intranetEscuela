86e43b703adb2baf37630c4c1fe3f39db2a6dfb9
Erik <erikpvc@gmail.com>
2021-06-11 14:08:00 +0200

    Es esto un mapeado???

--- principal.php
+++ principal.php
@@ -1,11 +1,34 @@
-<?php 
-if(!isset($_SESSION['level'])) {
+<?php include ('assets/includes/Header.php'); include ('assets/includes/Navigation.php');
+
+//  testeando el mapeado
+$page = filter_input(INPUT_GET, 'page');
+
+if (is_null($page)) {
     header('Location: index.php?mensaje=error_identificar');
-} else if ($_SESSION['level'] == 0) {
-    include ('assets/pages/Alumno.php');
-} else if ($_SESSION['level'] == 1) {
-    include ('assets/pages/Profesor.php');
-} else if ($_SESSION['level'] == 2) {
-    include ('assets/pages/Director.php');
 }
-include ('assets/includes/Footer.php');  ?>
+
+$page = strtr(
+    $page,
+    [
+        'alumno' => 'Alumno',
+        'profesor' => 'Profesor',
+        'director' => 'Director',
+    ]
+);
+
+$file_name = 'assets/pages/' . $page . '.php';
+
+if (isset($page)) {
+    // logout
+    if ($page == 'logout') {
+        header('Location: assets/php/Desconectar.php');
+    }
+
+    // other pages
+    if (file_exists($file_name)) {
+        include $file_name;
+    }
+}
+
+include ('assets/includes/Footer.php');  
+?>
